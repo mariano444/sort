719dadc21dd32333faaf5450f45789ce364e5359
mariano444 <80271490+mariano444@users.noreply.github.com>
2026-04-06 10:20:36 -0300

    Update supabase.ts

--- supabase/functions/_shared/supabase.ts
+++ supabase/functions/_shared/supabase.ts
@@ -30,5 +30,6 @@ export function corsHeaders() {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Max-Age": "86400",
   };
 }
